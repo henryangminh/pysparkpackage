b5d7be651cc781787732bc3d3da7cd3974b45f3f
henryangminh <hippore114@gmail.com>
2022-04-13 16:02:54 +0700

    add some new file to structure

--- setup.py
+++ setup.py
@@ -3,22 +3,27 @@ from setuptools import setup, find_packages
 with open('requirements.txt') as f: 
     requirements = f.readlines()
 
+with open('README.md') as f:
+    readme = f.read()
+
 setup( 
         name ='pysparkpackage', 
         version ='0.0.1', 
         author ='Minmin', 
-        # url ='https://github.com/Vibhu-Agarwal/vibhu4gfg', 
+        author_email = 'minmin@xxx.com.vn',
+        url = '',
         description ='Demo for pyspark', 
-        long_description_content_type ="text/markdown", 
+        long_description_content_type ="text/markdown",
+        long_description = readme,
         license='MIT', 
         packages=find_packages(), 
         entry_points ={ 
             'console_scripts': [ 
-                'hello-world = pysparkpackage.helper.hello_world.components:main',
+                'hello-world = pysparkpackage.helper.components.hello_world:main',
                 'transform = pysparkpackage.transformation.main:main'
             ] 
         }, 
         keywords ='pyspark package whl egg', 
-        install_requires = requirements, 
-        zip_safe = False
+        install_requires = requirements
 )
+
